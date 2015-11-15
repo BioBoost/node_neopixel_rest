ac91f0f882eb9b64e4a2d1e00d5f368dc52bcfb4
Nico De Witte <nico.dewitte@vives.be>
2015-11-15 17:22:46 +0100

    Add optional i2c. If not available we fake all info

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 
+// If i2c module is not available just ignore i2c
+var include_i2c = true;
+try {
+  var i2c = require('i2c');
+} catch(err) {
+  include_i2c = false;
+  console.log('Module i2c is not installed. Proceeding without');
+}
+
+if (include_i2c) {
+  var address = 0x40;
+  var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+}
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.json());   // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));   // for parsing application/x-www-form-urlencoded
@@ -21,7 +35,21 @@ app.get('/', function (req, res) {
 
 app.get('/strings', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ strings: [1, 2] }));
+
+  if (include_i2c) {
+    wire.readByte(function(err, nos) {
+      if (err) {
+        console.log('Could not read from i2c: ' + err);
+        res.send(JSON.stringify({ number_of_string: "", string_ids: [], status: "failed" }));
+      } else {
+        var ids = new Array(nos);
+        for (i = 0; i < nos; i++) { ids[i] = i; }
+        res.send(JSON.stringify({ number_of_string: nos, string_ids: ids, status: "success" }));
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ number_of_string: "2", string_ids: [0, 1], status: "success" }));
+  }
 });
 
 app.get('/strings/:id', function (req, res) {
@@ -39,7 +67,21 @@ app.post('/strings/:id', function (req, res) {
   console.log('Setting color for string ' + id + ' to ' + red + ' ' + green + ' ' + blue);
 
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ status: "success" }));
+
+  if (include_i2c) {
+    // Send color to mbed
+    wire.write([0x03, red, green, blue], function(err) {
+      if (err) {
+        console.log('Could not write color to i2c: ' + err);
+        res.send(JSON.stringify({ status: "failed" }));
+      } else {
+        res.send(JSON.stringify({ status: "success" }));
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ status: "success" }));
+  }
+
 });
 
 // Custom 404 (needs to be last in line of routes)
